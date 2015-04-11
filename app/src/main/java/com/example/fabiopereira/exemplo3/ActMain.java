024aa2b69232c32a58969d0efd4a3e7a996b76e3
fabiopereirafox <fabio.pereira@industriafox.com>
2015-04-11 15:06:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fabiopereira/exemplo3/ActMain.java
@@ -0,0 +1,18 @@
+package com.example.fabiopereira.exemplo3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fabio.pereira on 4/5/15.
+ */
+public class ActMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.act_main);
+
+    }
+
+}
